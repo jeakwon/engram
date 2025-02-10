9ac9b43280231bdb867f10825f1b15fae18a477c
Jea Kwon <33548421+jeakwon@users.noreply.github.com>
2025-02-10 18:58:32 +0100

    update

--- models.py
+++ models.py
@@ -3,15 +3,15 @@ import torch.nn as nn
 
 def cifar_resnet18(pretrained, num_classes):
     model = timm.create_model("resnet18", pretrained=pretrained)
-    model.fc = nn.Linear(512, num_classes)
+    model.fc = nn.Linear(model.fc.in_features, num_classes)
     return model
 
-def cifar_vit(pretrained, num_classes):
-    model = timm.create_model("vit_small_patch16_224", pretrained=pretrained)
+def cifar_vit_base_patch16_224(pretrained, num_classes):
+    model = timm.create_model("vit_base_patch16_224", pretrained=pretrained)
     model.head = nn.Linear(model.head.in_features, num_classes)
     return model
 
-def cifar_mixer(pretrained, num_classes):
+def cifar_mixer_b16_224(pretrained, num_classes):
     model = timm.create_model("mixer_b16_224", pretrained=pretrained)
     model.head = nn.Linear(model.head.in_features, num_classes)
     return model
