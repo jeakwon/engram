f727021c6393e61483f98e9345fe9406985ab185
Jea Kwon <33548421+jeakwon@users.noreply.github.com>
2025-02-10 18:37:16 +0100

    update

---
+++ train.py
@@ -0,0 +1,50 @@
+import torch
+
+def train_epoch(device, model, trainloader, criterion, optimizer, scheduler, mixup_fn):
+    model.train()
+    running_loss, correct, total = 0.0, 0, 0
+
+    for inputs, labels in trainloader:
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        # Apply MixUp
+        inputs, labels = mixup_fn(inputs, labels)
+
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # Track Loss & Accuracy
+        running_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += labels.size(0)
+        correct += predicted.eq(labels.argmax(dim=1)).sum().item()
+
+    scheduler.step()
+
+    running_loss /= len(trainloader)
+    accuracy = 100 * correct / total
+    return running_loss, accuracy
+
+
+@torch.no_grad()
+def test_epoch(device, model, testloader, criterion):
+    start_time = time.time()
+    model.eval()
+    running_loss, correct, total = 0.0, 0, 0
+    for inputs, labels in testloader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+
+        # Track Loss & Accuracy
+        running_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += labels.size(0)
+        correct += predicted.eq(labels).sum().item()
+    running_loss /= len(testloader)
+    accuracy = 100 * correct / total
+    duration = time.time() - start_time
+    return running_loss, accuracy, duration
