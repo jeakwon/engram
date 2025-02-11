a55b411de0cbace5897617de4b15a99adf3fa708
Jea Kwon <33548421+jeakwon@users.noreply.github.com>
2025-02-11 01:58:13 +0100

    update

--- models.py
+++ models.py
@@ -9,13 +9,7 @@ def cifar_resnet18(pretrained, num_classes, **kwargs):
     return model
 
 @register_model
-def cifar_vit_base_patch16_224(pretrained, num_classes, **kwargs):
-    model = timm.create_model("vit_base_patch16_224", pretrained=pretrained)
-    model.head = nn.Linear(model.head.in_features, num_classes)
-    return model
-
-@register_model
-def cifar_mixer_b16_224(pretrained, num_classes, **kwargs):
-    model = timm.create_model("mixer_b16_224", pretrained=pretrained)
+def cifar_vit_small_patch16_224(pretrained, num_classes, **kwargs):
+    model = timm.create_model("vit_small_patch16_224", pretrained=pretrained)
     model.head = nn.Linear(model.head.in_features, num_classes)
     return model
